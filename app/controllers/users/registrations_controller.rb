d4367bf559f28c8fad20b8f47f216c79e6ba00ab
Matt Griffith <mattgriffith0@gmail.com>
2021-11-07 15:49:07 -0800

    style edit profile page

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:edit, :update]
+  
   protected
 
   def update_resource(resource, account_update_params)
