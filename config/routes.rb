a7518508bea582d3773e99eced9ad98e2172adca
Matt Griffith <mattgriffith0@gmail.com>
2021-11-06 21:34:36 -0700

    modify users controller and routes and add views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
   resources :users, only: [:show, :index]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'main#index'
 end
