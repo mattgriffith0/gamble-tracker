8eda01f29f90ba0562f08ce12a1f038e4097970d
Matt Griffith <mattgriffith0@gmail.com>
2021-11-11 22:03:39 -0800

    add trips scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   }
 
   resources :users, only: [:show, :index]
-
+  resources :trips
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'main#index'
 end
