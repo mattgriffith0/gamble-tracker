74dd92261205dae5aba3abd3b1cb5116fd66772e
Matt Griffith <mattgriffith0@gmail.com>
2021-11-18 18:27:16 -0800

    add odds model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_19_022544) do
+ActiveRecord::Schema.define(version: 2021_11_19_022701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,12 @@ ActiveRecord::Schema.define(version: 2021_11_19_022544) do
     t.index ["casino_id"], name: "index_games_on_casino_id"
     t.index ["trip_id"], name: "index_games_on_trip_id"
     t.index ["user_id"], name: "index_games_on_user_id"
+  end
+
+  create_table "odds", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "trips", force: :cascade do |t|
